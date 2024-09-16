9594741ff3cad987b05255035876789a6e50968d
Sergio Melero <sergiomele97@gmail.com>
2024-09-16 16:47:46 +0200

    Se añade gif nyan cat #10

--- WebPage/linkedcrystalSW/src/app/landing/landing.component.ts
+++ WebPage/linkedcrystalSW/src/app/landing/landing.component.ts
@@ -10,24 +10,10 @@ export class LandingComponent {
 
   showGif() {
     this.isGifVisible = true;
-    console.log("Hola");
 
-    // Espera a que el DOM se actualice antes de agregar la clase 'show'
+    // Después de 5 segundos, oculta el GIF
     setTimeout(() => {
-      const gifContainer = document.querySelector('.gif-container');
-      if (gifContainer) {
-        gifContainer.classList.add('show');
-
-        // Espera 5 segundos y luego agrega la clase 'hide' para ocultar el GIF
-        setTimeout(() => {
-          gifContainer.classList.add('hide');
-          // Después de que la animación de ocultamiento termine, oculta el elemento completamente
-          setTimeout(() => {
-            this.isGifVisible = false;
-            gifContainer.classList.remove('show', 'hide');
-          }, 1000); // Tiempo de transición de desaparición
-        }, 5000); // Tiempo antes de ocultar (5 segundos)
-      }
-    }, 0); // Asegura que la clase 'show' se aplique después de cambiar 'isGifVisible'
+      this.isGifVisible = false;
+    }, 5000); // Tiempo durante el cual el GIF es visible
   }
 }
